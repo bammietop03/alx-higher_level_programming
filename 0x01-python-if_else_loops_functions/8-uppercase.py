8f85a4bf19034ee1b3ddf48c3537ce5020659622
bammietop03 <bammietop@gmail.com>
2023-09-05 13:53:45 +0100

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     uppercase = 0
     for i in str:
-        if(ord(i) >= 97 and ord(i) <= 122):
+        if ord(i) >= 97 and ord(i) <= 122:
             uppercase = 32
         else:
             uppercase = 0
