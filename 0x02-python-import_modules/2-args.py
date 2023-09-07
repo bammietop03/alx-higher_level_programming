61a66da568095286bfa84283804fec2105dc11d0
bammietop03 <bammietop@gmail.com>
2023-09-07 15:50:27 +0100

    python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,4 @@ if __name__ == "__main__":
         for i in range(1, argc):
             print("{}: {}".format(i, sys.argv[i]))
     else:
-        print("0 arguments.")
-
+        print("{} arguments.".format(argc - 1))
