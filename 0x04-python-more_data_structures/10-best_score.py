7c05e39d7f70fc6456c80831a69346e6c4fbb33e
bammietop03 <bammietop@gmail.com>
2023-09-13 19:11:33 +0100

    python-more_data_structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        best_key = max(a_dictionary, key=lambda k: a_dictionary[k])
+        return best_key
+    else:
+        return None
