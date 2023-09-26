03abee0b7bbbb99c7738655b4474fcd300f233a0
bammietop03 <bammietop@gmail.com>
2023-09-26 14:48:46 +0100

    python-classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class """
+
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
