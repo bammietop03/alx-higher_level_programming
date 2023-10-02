85cdd66e17dfa42247fac5fa5d9a574c67f7f859
bammietop03 <bammietop@gmail.com>
2023-10-02 19:58:02 +0100

    python-more_classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Rectangle:
+    """  that defines a rectangle """
+    pass#!/usr/bin/python3
+class Rectangle:
+    """  that defines a rectangle """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Learning Python More Classes
