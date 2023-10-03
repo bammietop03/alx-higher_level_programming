a0b01a4cd68b4bba30cb291ca9d751c601ce5af5
bammietop03 <bammietop@gmail.com>
2023-10-03 08:18:06 +0100

    python-test_driven_development

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+ function that prints a square with the character #
+"""
+
+
+def print_square(size):
+    """
+    Prints a square of '#' characters with a specified size.
+
+    Args:
+        size (int): The size (length) of the square.
+
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0.
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
