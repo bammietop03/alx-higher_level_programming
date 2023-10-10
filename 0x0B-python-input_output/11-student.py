ffe8572057f96fdd9db57655c2e82a1ff5cc526c
bammietop03 <bammietop@gmail.com>
2023-10-10 14:57:15 +0100

    python-input_output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module defines a class Student"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets a dictionary representation of the Student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+    
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
