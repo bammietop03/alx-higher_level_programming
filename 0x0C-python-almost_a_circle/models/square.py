e9d7d071caa97e6d85cf5dc0050f1ef3e85d4f1d
bammietop03 <bammietop@gmail.com>
2023-10-14 10:16:54 +0100

    python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Write the class Square that inherits from Rectangle """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ sqaure class created and inherits from rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initialization... """
+
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for i, value in enumerate(args):
+                setattr(self, attrs[i], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def __str__(self):
+        """ return [Square] (<id>) <x>/<y> - <size> """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
