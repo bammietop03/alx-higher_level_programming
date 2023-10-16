687ae668c19bebefcf12d698de2861b4970467bc
bammietop03 <bammietop@gmail.com>
2023-10-16 20:02:51 +0100

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,44 +1,58 @@
 #!/usr/bin/python3
-""" Write the class Square that inherits from Rectangle """
+"""Module that defines a square object"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ sqaure class created and inherits from rectangle """
+    """Defines a square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ initialization... """
-
+        """Method that initialized the square
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ return [Square] (<id>) <x>/<y> - <size> """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
-                                                 self.y, self.width)
+        """Method that returns a string"""
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                  self.width))
 
     @property
     def size(self):
+        """Getter the size of the square
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter the size of the square"""
         self.width = value
-        self.height = value
+        self.heigth = value
 
     def update(self, *args, **kwargs):
-        if args:
-            attrs = ['id', 'size', 'x', 'y']
-            for i, value in enumerate(args):
-                setattr(self, attrs[i], value)
+        """Method that update arguments for square object
+        """
+        dict_order = ['id', 'size', 'x', 'y']
+        if args is not None and bool(args) is True:
+            i = 0
+            for key in dict_order:
+                try:
+                    setattr(self, key, args[i])
+                except IndexError:
+                    pass
+                i += 1
         else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            for key in dict_order:
+                try:
+                    setattr(self, key, kwargs[key])
+                except KeyError:
+                    pass
 
     def to_dictionary(self):
-        """ returns a dictionary """
-        return {
-                "id": self.id,
-                "size": self.width,
-                "x": self.x,
-                "y": self.y
-                }
+        """Method that returns the dictionary
+           representation of a Square.
+        """
+        dict_order = ['id', 'x', 'size', 'y']
+        dict_attrs = {}
+        for key in dict_order:
+            dict_attrs[key] = getattr(self, key)
+        return dict_attrs
