3e99178031faffcbd4e0732dc8676f6616d7a625
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-12 16:11:35 +0100

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newdict = {};
-for (let key in dict) {
+const newdict = {};
+for (const key in dict) {
   if (newdict[dict[key]] === undefined) {
     newdict[dict[key]] = [key];
   } else {
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,15 +5,15 @@ const fs = require('fs');
 const [, , file1, file2, destination] = process.argv;
 
 fs.readFile(file1, 'utf8', (err, data1) => {
+  if (err) throw err;
+
+  fs.readFile(file2, 'utf8', (err, data2) => {
     if (err) throw err;
 
-    fs.readFile(file2, 'utf8', (err, data2) => {
-        if (err) throw err;
+    const concatenatedData = `${data1}${data2}`;
 
-        const concatenatedData = `${data1}${data2}`;
-
-        fs.writeFile(destination, concatenatedData, 'utf8', (err) => {
-            if (err) throw err;
-        });
+    fs.writeFile(destination, concatenatedData, 'utf8', (err) => {
+      if (err) throw err;
     });
+  });
 });
