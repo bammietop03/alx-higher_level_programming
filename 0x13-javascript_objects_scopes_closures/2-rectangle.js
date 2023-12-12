a58978b8cd015d25e9837104efa574cd99b0f603
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-12 14:11:36 +0100

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
