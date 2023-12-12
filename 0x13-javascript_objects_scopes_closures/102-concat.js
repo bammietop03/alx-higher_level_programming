ff25a258ed234cea81203ca6fbd711b05ba6c9be
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-12 16:09:50 +0100

    javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const [, , file1, file2, destination] = process.argv;
+
+fs.readFile(file1, 'utf8', (err, data1) => {
+    if (err) throw err;
+
+    fs.readFile(file2, 'utf8', (err, data2) => {
+        if (err) throw err;
+
+        const concatenatedData = `${data1}${data2}`;
+
+        fs.writeFile(destination, concatenatedData, 'utf8', (err) => {
+            if (err) throw err;
+        });
+    });
+});
