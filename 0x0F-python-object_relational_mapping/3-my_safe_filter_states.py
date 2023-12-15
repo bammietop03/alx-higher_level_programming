95e0788ad0ba2f6b9c7225879a119095fc62eeed
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-15 10:57:44 +0100

    python-object_relational_mapping1

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
 
-def main(args):
-    if len(args) != 5:
-        raise Exception("need 4 arguments!")
-    db = MySQLdb.connect(host='localhost',
-                         user=args[1],
-                         passwd=args[2],
-                         db=args[3])
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-        (args[4],))
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
-    main(sys.argv)
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
