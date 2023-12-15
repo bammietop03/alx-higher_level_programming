59e6d4903476545a1e33f980682c69a9d50ea80a
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-15 01:43:21 +0100

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Retrives the states table from database
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         port=3306, passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+                    ORDER BY states.id ASC".format(argv[4]))
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
