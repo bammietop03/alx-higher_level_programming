53f45eb77be61d548af639ca7db8a6fa3b269e67
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-15 10:46:03 +0100

    python-object_relational_mapping1

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,22 +10,27 @@ if __name__ == "__main__":
     """
     Retrives the states table from database
     """
-    if len(argv) != 5:
+    if len(sys.argv) != 5:
         print("Usage: script.py <username> <password> <database> <state_name>")
         sys.exit(1)
+    
+    try:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             port=3306, passwd=sys.argv[2], db=sys.argv[3])
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         port=3306, passwd=sys.argv[2], db=sys.argv[3])
+        cursor = db.cursor()
 
-    cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+                        ORDER BY states.id ASC".format(sys.argv[4]))
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-                    ORDER BY states.id ASC".format(sys.argv[4]))
+        data = cursor.fetchall()
 
-    data = cursor.fetchall()
+        for row in data:
+            print(row)
 
-    for row in data:
-        print(row)
+        cursor.close()
+        db.close()
 
-    cursor.close()
-    db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
