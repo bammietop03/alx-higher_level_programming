a1cb786efa4686a1fa6da2135c9997331d62b172
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-15 10:52:35 +0100

    python-object_relational_mapping1

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""
-a script that lists all states from the database hbtn_0e_0_usa
-"""
+# gets all states via python yee boi with your own state SAFE
 
-import MySQLdb
-from sys import argv
+
+def main(args):
+    # gets all state stuff SAFELY
+    if len(args) != 5:
+        raise Exception("need 4 arguments!")
+    db = MySQLdb.connect(host='localhost',
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+        (args[4],))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
 
 if __name__ == "__main__":
-    """
-    Retrives the states table from database
-    """
-    if len(argv) != 5:
-        raise Exception("need 4 arguments!")
-
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         port=3306, passwd=argv[2], db=argv[3])
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-                    ORDER BY states.id ASC".format(argv[4]))
-
-    data = cursor.fetchall()
-
-    for row in data:
-        print(row)
-
-    cursor.close()
-    db.close()
+    import sys
+    import MySQLdb
+    main(sys.argv)
