d6e0f4d60bd4309083e091eae4a0d373af4936c5
Bamiwo Adebayo <bammietop@gmail.com>
2024-01-07 14:10:32 +0100

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """This script fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         data = response.read()
