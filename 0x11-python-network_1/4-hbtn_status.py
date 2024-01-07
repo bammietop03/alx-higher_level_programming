4512defeafe706fb99d4b1f7186a7e37fd1536f4
Bamiwo Adebayo <bammietop@gmail.com>
2024-01-07 18:03:22 +0100

    python network2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-a Python script that fetches https://alx-intranet.hbtn.io/status
-"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    data = requests.get("https://alx-intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(data.text)))
-    print("\t- contnent: {}".format(data.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
